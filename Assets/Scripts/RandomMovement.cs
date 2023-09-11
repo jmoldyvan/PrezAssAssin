f2abd015c5c19ba65a5d14ee6ca232d87e65226c
jordan moldovan <jmoldyvan@gmail.com>
2023-09-10 22:57:03 -0700

    start menu

--- Assets/Scripts/RandomMovement.cs
+++ Assets/Scripts/RandomMovement.cs
@@ -1,193 +1,3 @@
-// using System.Collections;
-// using UnityEngine;
-
-// public class RandomMovement : MonoBehaviour
-// {
-//     public float minX = 0f;
-//     public float maxX = 30f;
-//     public float minY = 0f;
-//     public float maxY = 20f;
-//     public float moveSpeed = 2f;
-
-//     private Vector3 targetPosition;
-
-//     void Start()
-//     {
-//         PickNewTarget();
-//         StartCoroutine(MoveToTarget());
-//     }
-
-//     void Update()
-//     {
-//         // Empty for now, can add collision logic later
-//     }
-
-//     private IEnumerator MoveToTarget()
-//     {
-//         while (true)
-//         {
-//             float distance = Vector3.Distance(transform.position, targetPosition);
-//             if (distance > 0.01f)
-//             {
-//                 transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
-//             }
-//             else
-//             {
-//                 PickNewTarget();
-//             }
-//             yield return null;
-//         }
-//     }
-
-//     private void PickNewTarget()
-//     {
-//         targetPosition = new Vector3(Random.Range(minX, maxX), Random.Range(minY, maxY), transform.position.z);
-//     }
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// using System.Collections;
-// using UnityEngine;
-
-// public class RandomMovement : MonoBehaviour
-// {
-//     public float minX = 0f;
-//     public float maxX = 30f;
-//     public float minY = 0f;
-//     public float maxY = 20f;
-//     public float moveSpeed = 2f;
-//     public float moveDelay = 1f;
-//     public float radiusMultiplier = 1.5f;
-//     public float repulsionRadius = 1f;
-//     public float repulsionForce = 2f;
-
-//     private Vector3 originalDirection;
-//     private bool collided = false;
-//     private Vector3 shuffleCenter;
-//     private int movementType;
-//     private Vector3 lastCollisionNormal;
-
-//     void Start()
-//     {
-//         transform.position = new Vector3(transform.position.x, transform.position.y, 1);
-//         movementType = Random.Range(0, 2);
-//         shuffleCenter = transform.position;
-//         originalDirection = GetRandomDirection();
-        
-//         StartCoroutine(StartMovementPatterns());
-//     }
-
-//     void FixedUpdate()
-//     {
-//         // Fix the Z-axis position
-//         transform.position = new Vector3(transform.position.x, transform.position.y, 1);
-
-//         // Add repulsion logic
-//         Collider2D[] hitColliders = Physics2D.OverlapCircleAll(transform.position, repulsionRadius);
-//         Vector3 repulsionVector = Vector3.zero;
-
-//         foreach (var hitCollider in hitColliders)
-//         {
-//             if (hitCollider.gameObject != gameObject)
-//             {
-//                 Vector3 toCollider = hitCollider.transform.position - transform.position;
-//                 repulsionVector -= toCollider.normalized / toCollider.magnitude;
-//             }
-//         }
-
-//         transform.position += repulsionVector * repulsionForce * Time.deltaTime;
-//     }
-
-//     private IEnumerator StartMovementPatterns()
-//     {
-//         while (true)
-//         {
-//             if (collided)
-//             {
-//                 yield return new WaitUntil(() => !collided);
-//             }
-
-//             switch (movementType)
-//             {
-//                 case 0:
-//                     yield return StartCoroutine(Shuffle());
-//                     break;
-//                 case 1:
-//                     yield return StartCoroutine(Shuffle());
-//                     break;
-//             }
-
-//             yield return new WaitForSeconds(moveDelay);
-//         }
-//     }
-
-//     private IEnumerator Shuffle()
-//     {
-//         Vector3 targetPosition = shuffleCenter + new Vector3(Random.Range(-radiusMultiplier, radiusMultiplier), Random.Range(-radiusMultiplier, radiusMultiplier), 0f);
-//         yield return StartCoroutine(MoveToPosition(targetPosition));
-//     }
-
-//     private IEnumerator MoveToPosition(Vector3 targetPosition)
-//     {
-//         float distance = Vector3.Distance(transform.position, targetPosition);
-//         while (distance > 0.01f)
-//         {
-//             if (collided)
-//             {
-//                 yield break;
-//             }
-//             transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
-//             distance = Vector3.Distance(transform.position, targetPosition);
-//             yield return null;
-//         }
-//     }
-
-//     private void OnCollisionEnter2D(Collision2D collision)
-//     {
-//         collided = true;
-//         lastCollisionNormal = collision.contacts[0].normal;  // Store the collision normal
-//         StartCoroutine(HandleCollision());
-//         Debug.Log($"Collision detected with: {collision.gameObject.name}");
-//     }
-
-//     private IEnumerator HandleCollision()
-//     {
-//         yield return new WaitForSeconds(1f);
-//         originalDirection = Vector3.Reflect(originalDirection, lastCollisionNormal);  // Reflect based on last collision
-//         collided = false;
-//     }
-
-//     private Vector3 GetRandomDirection()
-//     {
-//         return new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 0f).normalized;
-//     }
-// }
-
-
-
-
-
-
-
-
 using System.Collections;
 using UnityEngine;
 
@@ -207,6 +17,11 @@ public class RandomMovement : MonoBehaviour
     private bool hasBounced = false;
     private float timer = 0f;
 
+    // Additional variables for backup maneuver
+    private bool inBackupMode = false;
+    private Vector2 backupDirection;
+    private float backupTime = 2f;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -218,13 +33,12 @@ public class RandomMovement : MonoBehaviour
 
     void FixedUpdate()
     {
-        if (isMoving && !collided)
+        if (isMoving && !collided && !inBackupMode)
         {
             float step = moveSpeed * Time.fixedDeltaTime;
             Vector3 newPosition = Vector3.MoveTowards(transform.position, targetPosition, step);
             rb.MovePosition(newPosition);
 
-            // Update sprite orientation
             if (newPosition.x > transform.position.x)
             {
                 transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
@@ -234,7 +48,6 @@ public class RandomMovement : MonoBehaviour
                 transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
             }
 
-            // Reset the inactivity timer if the object has moved
             if (transform.position != lastPosition)
             {
                 timer = 0f;
@@ -245,18 +58,23 @@ public class RandomMovement : MonoBehaviour
             float step = moveSpeed * Time.fixedDeltaTime;
             rb.MovePosition(Vector3.MoveTowards(transform.position, startPosition, step));
         }
+        
+        if (inBackupMode)
+        {
+            float step = moveSpeed * Time.fixedDeltaTime;
+            Vector3 newPosition = transform.position + (Vector3)backupDirection * step;
+            rb.MovePosition(newPosition);
+        }
 
-        // Update the last position
         lastPosition = transform.position;
-
-        // Update the inactivity timer
         timer += Time.fixedDeltaTime;
+
         if (timer >= inactivityTime)
         {
-            // Reset the object's movement
-            StopCoroutine(StartMovementPatterns());
-            StartCoroutine(StartMovementPatterns());
             timer = 0f;
+            inBackupMode = true;
+            backupDirection = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized;
+            StartCoroutine(BackupManeuver());
         }
     }
 
@@ -288,7 +106,6 @@ public class RandomMovement : MonoBehaviour
             collided = true;
             isMoving = false;
 
-            // Move away from the wall slightly
             Vector2 collisionNormal = collision.GetContact(0).normal;
             rb.MovePosition(rb.position - collisionNormal * 0.1f);
 
@@ -302,5 +119,13 @@ public class RandomMovement : MonoBehaviour
         yield return new WaitUntil(() => Vector3.Distance(transform.position, startPosition) < 0.1f);
         collided = false;
         isMoving = true;
+    }
+
+    private IEnumerator BackupManeuver()
+    {
+        yield return new WaitForSeconds(backupTime);
+        inBackupMode = false;
+        StopCoroutine(StartMovementPatterns());
+        StartCoroutine(StartMovementPatterns());
     }
 }
