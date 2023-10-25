0028f8ab46d0193ae9cf6fda69e44e50cb4fe175
jordan moldovan <jmoldyvan@gmail.com>
2023-10-24 20:22:48 -0700

    added player spawn boundry

--- Assets/Scripts/MenuAndtransition/AfterGunBarrelToPhase2.cs
+++ Assets/Scripts/MenuAndtransition/AfterGunBarrelToPhase2.cs
@@ -33,9 +33,37 @@ public class AfterGunBarrelToPhase2 : StateMachineBehaviour
 
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     
-    {
+    {Debug.Log("OnStateExit method called!");
+
+        // 1. Locate the PlayerSpawnBoundary GameObject
+        GameObject playerAndHatchSpawnObjects = GameObject.Find("PlayerAndHatchSpawnObjects");
+        Debug.Log(playerAndHatchSpawnObjects);
+        Transform playerSpawnBoundaryTransform = playerAndHatchSpawnObjects.transform.Find("PlayerSpawnBoundry");
+        Debug.Log(playerSpawnBoundaryTransform);
+        
+        // 2. Find the 4 child objects
+
+        Transform topLeft = playerSpawnBoundaryTransform.Find("PlayerCornerTopLeft");
+        Debug.Log("topLeft: " + topLeft);
+        Transform topRight = playerSpawnBoundaryTransform.Find("PlayerCornerTopRight");
+        Debug.Log("topRight: " + topRight);
+        Transform bottomLeft = playerSpawnBoundaryTransform.Find("PlayerCornerBottomLeft");
+        Debug.Log("bottomLeft: " + bottomLeft);
+        Transform bottomRight = playerSpawnBoundaryTransform.Find("PlayerCornerBottomRight");
+        Debug.Log("bottomRight: " + bottomRight);
+
+        
+        // 3. Retrieve the x and y coordinates
+        float PlayerSpawnXmin = bottomLeft.position.x;
+        float PlayerSpawnXmax = topRight.position.x;
+        float PlayerSpawnYmin = bottomLeft.position.y;
+        float PlayerSpawnYmax = topLeft.position.y;
+
+        float PlayerRandomXCord = Random.Range(PlayerSpawnXmin, PlayerSpawnXmax);
+        float PlayerRandomYCord = Random.Range(PlayerSpawnYmin, PlayerSpawnYmax);
         Vector3 exitDoorPosition = new Vector3(72f, 21f, 0);
-        Vector3 playerPosition = new Vector3(-3f, 12f, 1.5f); 
+
+        Vector3 playerPosition = new Vector3(PlayerRandomXCord, PlayerRandomYCord, 1.5f); 
         Vector3 Phase2ButtonPosition = new Vector3(25f, 25f, -6); 
         Quaternion rotation = Quaternion.identity;
 
