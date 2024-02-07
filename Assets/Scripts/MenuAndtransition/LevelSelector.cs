65f86655d357fe4e4d183622ec5d829d5eace27d
jordan moldovan <jmoldyvan@gmail.com>
2024-02-07 15:52:28 -0800

    added blacked out disabled level select

--- Assets/Scripts/MenuAndtransition/LevelSelector.cs
+++ Assets/Scripts/MenuAndtransition/LevelSelector.cs
@@ -1,14 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LevelSelector : MonoBehaviour
 {
     public Loaders transition;
+    public Button[] LevelButtons;
     public void Select (int levelName)
     {
         transition.StartCoroutine(transition.LoadLevel(levelName));
     }
 
+    void Start()
+    {
+        for (int i = 0; i < LevelButtons.Length; i++)
+        {
+            LevelButtons[i].interactable = false;
+        }
+    }
 
 }
